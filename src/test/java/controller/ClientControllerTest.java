62c1c95d3253ee7c65f240542adc7af2af21bdb9
Adrian Precupeanu <adrianprecupeanu@gmail.com>
2018-04-01 11:44:05 +0300

    L2 created tests

---
+++ src/test/java/controller/ClientControllerTest.java
@@ -0,0 +1,18 @@
+package controller;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Adrian on 01.04.2018.
+ */
+public class ClientControllerTest extends TestCase {
+    public void testAddClient() throws Exception {
+    }
+
+    public void testAddClientIndex() throws Exception {
+    }
+
+    public void testListIssue() throws Exception {
+    }
+
+}
---
+++ src/test/java/repository/DataManagerTest.java
@@ -0,0 +1,18 @@
+package repository;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Adrian on 01.04.2018.
+ */
+public class DataManagerTest extends TestCase {
+    public void testSaveChanges() throws Exception {
+    }
+
+    public void testGetInvoicesList() throws Exception {
+    }
+
+    public void testGetClientsList() throws Exception {
+    }
+
+}
