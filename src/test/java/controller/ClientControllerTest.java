1b50b46a0e85fb3ae8395cabe90aa666f24e6fb5
Adrian Precupeanu <adrianprecupeanu@gmail.com>
2018-04-03 08:38:24 +0300

    added test case

--- src/test/java/controller/ClientControllerTest.java
+++ src/test/java/controller/ClientControllerTest.java
@@ -11,7 +11,7 @@ public class ClientControllerTest extends TestCase {
     public void testAddClient() throws Exception {
 //        assertTrue(true);
 
-        System.out.println("\nT E S T   >>  Add New Product\n");
+        System.out.println("\n ---------------------- Add New Client\n");
 
         ClientController con = new ClientController();
 //        assertEquals(2, con._dataManager.Clients.size());
@@ -30,7 +30,7 @@ public class ClientControllerTest extends TestCase {
 
     public void testAddClientIndex() throws Exception {
 
-        System.out.println("\nT E S T   >>  Add New Issue\n");
+        System.out.println("\n ---------------------- Add New Issue\n");
 
         ClientController con = new ClientController();
         assertEquals(0, con._dataManager.Issues.size());
@@ -47,6 +47,31 @@ public class ClientControllerTest extends TestCase {
         System.out.println(issue.Paid == 200);
         assert (issue.Paid == 200);
         con.AddClientIndex(issue.Client, issue.Year, issue.Month, issue.ToPay);
+//        assertEquals(0, con._dataManager.Issues.size());
+        System.out.println(issue.toString());
+    }
+
+    public void testListIssues() throws Exception {
+
+        System.out.println("\n ---------------------- List Issues of Client\n");
+
+        ClientController con = new ClientController();
+//        assertEquals(2, con._dataManager.Clients.size());
+        Client client = new Client("L", "l1", "12");
+
+        System.out.println(client.Name.equals("L"));
+        assert (client.Name.equals("L"));
+        System.out.println(client.Address.equals("l1"));
+        assert (client.Address.equals("l1"));
+        System.out.println(client.idClient.equals("12"));
+        assert (client.idClient.equals("12"));
+        con.AddClient(client.Name, client.Address, client.idClient);
+//		assertEquals(2, con._dataManager.Clients.size());
+        System.out.println(client.toString());
+
+        assertEquals(0, con._dataManager.Issues.size());
+        Issue issue = new Issue("12", 2012, 8, 400, 200);
+        String result = con.ListIssue("12");
 //        assertEquals(0, con._dataManager.Issues.size());
         System.out.println(issue.toString());
     }
